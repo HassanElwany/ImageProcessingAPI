bba6af9371fa0222c082763f4f279dd9b37787ab
Hassan Elwany <elwany_hassan@hotmail.com>
2022-03-17 19:35:34 +0300

    build server and run the first jasmine test

--- resized/index.js
+++ resized/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log('hello world');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 3000;
+// build rout
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello world '
+    });
+});
+// listening to server 
+app.listen(PORT, () => {
+    console.log(`server is listening on port: ${PORT}`);
+});
+exports.default = app;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('hello world')
+import express, {Application, Request, Response} from 'express'
+
+const app: Application = express();
+
+const PORT = 3000
+
+// build rout
+app.get('/', (req: Request, res: Response) => {
+    res.json ({
+        message: 'Hello world '
+    });
+});
+
+// listening to server 
+app.listen (PORT, () => {
+    console.log(`server is listening on port: ${PORT}`)
+});
+
+
+export default app;
+
+
